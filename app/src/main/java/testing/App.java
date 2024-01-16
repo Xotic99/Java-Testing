4af42b497cb9e0035578048fcd90234bde780386
Landon L <126203412+Xotic99@users.noreply.github.com>
2024-01-16 13:11:03 +0000

    update

--- app/src/main/java/testing/App.java
+++ app/src/main/java/testing/App.java
@@ -1,26 +1,9 @@
 package testing;
 
-import java.util.Scanner;
-
-
 public class App {
     public static void main(String[] args) {
-        Shirt favShirt = new Shirt("large", 10.75, true);
-    Scanner input = new Scanner(System.in);
+        
+            System.out.println("Hello World!");
 
-    System.out.println("Favorite Shirt: Original");
-    System.out.println("Shirt Size: " + favShirt.getPrice());
-    System.out.println("Shirt Price: " + favShirt.getPrice());
-    System.out.println("Shirt Has Buttons: " + favShirt.getHasButtons());
-
-    System.out.println("--------------------");
-
-    favShirt.setSize("small");
-    favShirt.setHasButtons(false);
-
-    System.out.println("Favorite Shirt: Updated");
-    System.out.println("Shirt Size: " + favShirt.getPrice());
-    System.out.println("Shirt Price: " + favShirt.getPrice());
-    System.out.println("Shirt Has Buttons: " + favShirt.getHasButtons());
     }
 }
